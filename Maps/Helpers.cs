356e65b87f3e3e6365bdaab0c45a8a714f1719ef
Sergi Ortiz Gómez <drualcman@msn.com>
2020-12-20 08:57:57 +0800

    fix simulate position. add readme

--- Maps/Helpers.cs
+++ Maps/Helpers.cs
@@ -11,14 +11,15 @@ namespace Maps
         /// </summary>
         /// <param name="start"></param>
         /// <returns></returns>
-        public static LatLongGPS SimulatePosition(int start)
+        public static LatLongGPS SimulatePosition(LatLongGPS start)
         {
-            Random random = new Random(start);
+            int point = Convert.ToInt32(start.Latitude + start.Longitude);
+            Random random = new Random(point);
             double distance = 0.01 + random.NextDouble() * 0.02;
             double angle = random.NextDouble() * Math.PI * 2;
             double latitude = distance * Math.Cos(angle);
             double longitude = distance * Math.Sin(angle);
-            return new LatLongGPS { Latitude = latitude, Longitude = longitude };
+            return new LatLongGPS { Latitude = start.Latitude + latitude, Longitude = start.Longitude + longitude };
         }
     }
 }
