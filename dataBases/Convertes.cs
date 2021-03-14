2e06a1cae5d749b989c0f7e80cf35de0186a1083
Sergi Ortiz Gómez <drualcman@msn.com>
2021-03-14 20:34:26 +0800

    add converter datatable to list

--- dataBases/Convertes.cs
+++ dataBases/Convertes.cs
@@ -182,6 +182,34 @@ namespace drualcman
             }
         }
 
+        public static List<T> DataTableToList<T>(DataTable dt)
+        {
+            List<T> data = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                T item = GetItem<T>(row);
+                data.Add(item);
+            }
+            return data;
+        }
+        private static T GetItem<T>(DataRow dr)
+        {
+            Type temp = typeof(T);
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (DataColumn column in dr.Table.Columns)
+            {
+                foreach (System.Reflection.PropertyInfo pro in temp.GetProperties())
+                {
+                    if (pro.Name == column.ColumnName)
+                        pro.SetValue(obj, dr[column.ColumnName], null);
+                    else
+                        continue;
+                }
+            }
+            return obj;
+        } 
+
         /// <summary>
         /// Convert XML String in DataSet
         /// </summary>
