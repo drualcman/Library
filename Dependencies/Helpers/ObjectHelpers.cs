a7a2f231b9a82ae8bc71e85938bc958c61b980df
Sergi Ortiz Gómez <drualcman@msn.com>
2021-05-16 16:47:56 +0800

    add objecthelpers class

---
+++ Dependencies/Helpers/ObjectHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drualcman.Helpers
+{
+    public class ObjectHelpers
+    {
+        public static bool IsGenericList(object o)
+        {
+            var oType = o.GetType();
+            return (oType.IsGenericType && (oType.GetGenericTypeDefinition() == typeof(List<>)));
+        }
+        public static bool IsGenericList<T>(object o)
+        {
+            var oType = o.GetType();
+            return (oType.IsGenericType && (oType.GetGenericTypeDefinition() == typeof(List<T>)));
+        }
+    }
+}
