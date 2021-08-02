5506d4c2fe5d563dcb7ba6aca7bc41b2f4a580e5
Sergi Ortiz Gómez <drualcman@msn.com>
2021-08-02 09:36:49 +0800

    have a look why can't read now :O in lists

--- dataBases/Lists.cs
+++ dataBases/Lists.cs
@@ -23,7 +23,64 @@ namespace drualcman
         /// </returns>
         public List<TModel> List<TModel>(string sql = "", int timeout = 30) where TModel : new()
         {
-            return ListAsync<TModel>(sql, timeout).Result;
+            defLog log = new defLog(this.FolderLog);
+            log.start("ToList", sql, "");
+
+            #region query
+            // If a query is empty create the query from the Model
+            if (string.IsNullOrWhiteSpace(sql))
+            {
+                sql = SetQuery<TModel>();
+            }
+            #endregion
+
+            try
+            {
+                using SqlDataReader dr = Reader(sql, timeout);
+
+                List<TModel> result = new List<TModel>();
+                if (dr is not null)
+                {
+                    if (dr.HasRows)
+                    {
+                        Type model = typeof(TModel);
+
+                        int tableCount = 0;
+                        if (TableNames is null)
+                        {
+                            TableNames = new List<TableName>();
+                            TableName table = new TableName(model.Name, $"t{tableCount}", string.Empty, InnerDirection.NONE, string.Empty, string.Empty, model.Name);
+                            TableNames.Add(table);
+                        }
+
+                        ColumnsHelpers ch = new ColumnsHelpers();
+
+                        List<string> hasList = new List<string>();
+                        ReadOnlyCollection<DbColumn> columnNames = dr.GetColumnSchema();
+                        List<Columns> columns = ch.HaveColumns(columnNames, model, $"t{tableCount}", TableNames);
+
+                        while (dr.Read())
+                        {
+                            TModel dat = ch.ColumnToObject<TModel>(dr, model, TableNames, tableCount, hasList, columns);
+                            result.Add(dat);
+                        }
+
+                        //if (hasList.Any())
+                        //{
+                        //    //need to create a list of object who is named in the list
+                        //    //1. create a list grouped by main model
+                        //    //List<TModel> mainModel = result.g;
+
+                        //}
+                    }
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                log.end(sql, ex.ToString() + "\n" + this.rutaDDBB);
+                throw;
+            }
         }
         #endregion
 
@@ -47,69 +104,8 @@ namespace drualcman
         /// Devuelve los datos de la consulta en un DataSet
         /// Si hay error devuelve el mensaje con el error
         /// </returns>
-        public async Task<List<TModel>> ListAsync<TModel>(string sql, int timeout = 30) where TModel : new()
-        {
-            defLog log = new defLog(this.FolderLog);
-            log.start("ToList", sql, "");
-
-            #region query
-            // If a query is empty create the query from the Model
-            if (string.IsNullOrWhiteSpace(sql))
-            {
-                sql = SetQuery<TModel>();
-            }
-            #endregion
-
-            try
-            {
-                using SqlDataReader dr = await this.ReaderAsync(sql, timeout);
-
-                List<TModel> result = new List<TModel>();
-                if (dr is not null)
-                {
-                    if (dr.HasRows)
-                    {
-                        Type model = typeof(TModel);
-
-                        int tableCount = 0;
-                        if (TableNames is null)
-                        {
-                            TableNames = new List<TableName>();
-                            TableName table = new TableName(model.Name, $"t{tableCount}", string.Empty, InnerDirection.NONE, string.Empty, string.Empty, model.Name);
-                            TableNames.Add(table);
-                        }
-                        else tableCount = TableNames.Count;
-
-
-                        ColumnsHelpers ch = new ColumnsHelpers();
-
-                        List<string> hasList = new List<string>();
-                        ReadOnlyCollection<DbColumn> columnNames = await dr.GetColumnSchemaAsync();
-                        List<Columns> columns = ch.HaveColumns(columnNames, model, $"t{tableCount}", TableNames);
-
-                        while (dr.Read())
-                        {
-                            TModel dat = ch.ColumnToObject<TModel>(dr, model, TableNames, tableCount, hasList, columns);                            
-                            result.Add(dat);
-                        }
-
-                        //if (hasList.Any())
-                        //{
-                        //    //need to create a list of object who is named in the list
-                        //    //1. create a list grouped by main model
-                        //    //List<TModel> mainModel = result.g;
-
-                        //}
-                    }
-                }
-                return result;
-            }
-            catch (Exception ex)
-            {
-                log.end(sql, ex.ToString() + "\n" + this.rutaDDBB);
-                throw;
-            }
-        }
+        public async Task<List<TModel>> ListAsync<TModel>(string sql, int timeout = 30) where TModel : new() =>
+            await Task.FromResult(List<TModel>(sql, timeout));
         #endregion
     }
 }
--- dataBases/SQLComands.cs
+++ dataBases/SQLComands.cs
@@ -99,11 +99,65 @@ namespace drualcman
             return result;
         }
 
-        /// <summary>
-        /// Execute a command and return the value
-        /// </summary>
-        /// <param name="sql">Query to execute</param>
-        /// <returns></returns>
+
+        public SqlDataReader Reader(string sql, int timeout = 30)
+        {
+            defLog log = new defLog(this.FolderLog);
+            SqlDataReader result;
+            log.start("Reader(sql)", sql, this.rutaDDBB);
+            #region query
+            // If a query is empty create the query from the Model
+            // check the sql if have some injection throw a exception
+            CheckSqlInjection(sql, log);
+            #endregion
+
+            try
+            {
+                this.OpenConnection();
+                using SqlCommand cmd = this.DbConnection.CreateCommand();
+                cmd.CommandText = sql;
+                result = Reader(cmd, timeout);
+            }
+            catch (Exception ex)
+            {
+                result = null;
+                log.end(result, ex);
+            }
+            return result;
+        }
+
+        public SqlDataReader Reader(SqlCommand cmd, int timeout = 30)
+        {
+            defLog log = new defLog(this.FolderLog);
+            SqlDataReader result;
+            log.start("Reader(cmd)", cmd.CommandText, this.rutaDDBB);
+            try
+            {
+                if (cmd.Connection == null)
+                {
+                    this.OpenConnection();
+                    cmd.Connection = this.DbConnection;
+                }
+                else
+                {
+                    if (cmd.Connection.State != System.Data.ConnectionState.Open)
+                        cmd.Connection.Open();
+                }
+                cmd.CommandTimeout = timeout;
+                result = cmd.ExecuteReader();
+                if (this.LogResults) log.end(result);
+            }
+            catch (Exception ex)
+            {
+                result = null;
+                log.end(result, ex);
+            }
+            finally
+            {
+                cmd.Dispose();
+            }
+            return result;
+        }
         #endregion
 
         #region task
@@ -186,64 +240,11 @@ namespace drualcman
             return result;
         }
 
-        public async Task<SqlDataReader> ReaderAsync(string sql, int timeout = 30)
-        {
-            defLog log = new defLog(this.FolderLog);
-            SqlDataReader result;
-            log.start("ReaderAsync(sql)", sql, this.rutaDDBB);
-            #region query
-            // If a query is empty create the query from the Model
-            // check the sql if have some injection throw a exception
-            CheckSqlInjection(sql, log);
-            #endregion
+        public async Task<SqlDataReader> ReaderAsync(string sql, int timeout = 30) =>
+            await Task.FromResult(Reader(sql, timeout));
 
-            try
-            {
-                await this .OpenConnectionAsync();
-                using SqlCommand cmd = this.DbConnection.CreateCommand();
-                cmd.CommandText = sql;
-                result = await ReaderAsync(cmd, timeout);
-            }
-            catch (Exception ex)
-            {
-                result = null;
-                log.end(result, ex);
-            }
-            return result;
-        }
-
-        public async Task<SqlDataReader> ReaderAsync(SqlCommand cmd, int timeout = 30)
-        {
-            defLog log = new defLog(this.FolderLog);
-            SqlDataReader result;
-            log.start("ReaderAsync(cmd)", cmd.CommandText, this.rutaDDBB);
-            try
-            {
-                if (cmd.Connection == null)
-                {
-                    await this.OpenConnectionAsync();
-                    cmd.Connection = this.DbConnection;
-                }
-                else
-                {
-                    if (cmd.Connection.State != System.Data.ConnectionState.Open)
-                        await cmd.Connection.OpenAsync();
-                }
-                cmd.CommandTimeout = timeout;
-                result =  cmd.ExecuteReader();
-                if (this.LogResults) log.end(result);
-            }
-            catch (Exception ex)
-            {
-                result = null;
-                log.end(result, ex);
-            }
-            finally
-            {
-                await cmd.DisposeAsync();
-            }
-            return result;
-        }
+        public async Task<SqlDataReader> ReaderAsync(SqlCommand cmd, int timeout = 30) =>
+            await Task.FromResult(Reader(cmd, timeout));
         #endregion
     }
 
