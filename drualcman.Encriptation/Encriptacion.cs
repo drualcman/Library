eb2e3c3c26a5c7027a53c4b0ab08dfe94fb0e117
Sergi Ortiz Gómez <drualcman@msn.com>
2022-12-19 11:20:06 +0800

    fixed invoicedownload and publish new version

--- drualcman.Encriptation/Encriptacion.cs
+++ drualcman.Encriptation/Encriptacion.cs
@@ -86,21 +86,24 @@ namespace drualcman
             {
                 if(string.IsNullOrEmpty(EncryptionKey) || string.IsNullOrWhiteSpace(EncryptionKey)) EncryptionKey = "!@#$%^&*()";
                 //EncryptionKey = Encriptacion.GetMD5(EncryptionKey);
-                cipherText = cipherText.Replace(" ", "+").Replace("~", "/");
-                byte[] cipherBytes = Convert.FromBase64String(cipherText);
-                using(Aes encryptor = Aes.Create())
+                if(!string.IsNullOrEmpty(cipherText))
                 {
-                    Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, 69);
-                    encryptor.Key = pdb.GetBytes(32);
-                    encryptor.IV = pdb.GetBytes(16);
-                    using(MemoryStream ms = new MemoryStream())
+                    cipherText = cipherText.Replace(" ", "+").Replace("~", "/");
+                    byte[] cipherBytes = Convert.FromBase64String(cipherText);
+                    using(Aes encryptor = Aes.Create())
                     {
-                        using(CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                        Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, 69);
+                        encryptor.Key = pdb.GetBytes(32);
+                        encryptor.IV = pdb.GetBytes(16);
+                        using(MemoryStream ms = new MemoryStream())
                         {
-                            cs.Write(cipherBytes, 0, cipherBytes.Length);
-                            cs.Close();
+                            using(CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                            {
+                                cs.Write(cipherBytes, 0, cipherBytes.Length);
+                                cs.Close();
+                            }
+                            cipherText = Encoding.Unicode.GetString(ms.ToArray());
                         }
-                        cipherText = Encoding.Unicode.GetString(ms.ToArray());
                     }
                 }
             }
