2502dd293a90a3a7772ec6b0581ec1e57e554376
Sergi Ortiz Gómez <drualcman@msn.com>
2023-03-22 10:01:34 +0800

    fixed warning in RFC2898DeriveBytes constructor

--- drualcman.Encriptation/Encriptacion.cs
+++ drualcman.Encriptation/Encriptacion.cs
@@ -63,7 +63,7 @@ namespace drualcman
 
             using(Aes encryptor = Aes.Create())
             {
-                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, 69);
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, 69, HashAlgorithmName.SHA256);
                 encryptor.Key = pdb.GetBytes(32);
                 encryptor.IV = pdb.GetBytes(16);
                 using(MemoryStream ms = new MemoryStream())
@@ -92,7 +92,7 @@ namespace drualcman
                     byte[] cipherBytes = Convert.FromBase64String(cipherText);
                     using(Aes encryptor = Aes.Create())
                     {
-                        Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, 69);
+                        Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, 69, HashAlgorithmName.SHA256);
                         encryptor.Key = pdb.GetBytes(32);
                         encryptor.IV = pdb.GetBytes(16);
                         using(MemoryStream ms = new MemoryStream())
